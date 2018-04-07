8db8b0af9141391f74d28bb9c6d321029d43575e
Marc Jean-Gilles <marcjeangilles@gmail.com>
2018-04-07 18:32:25 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,7 @@
- var playlist = {};
+var playlist = {
+  Slowdive:"Alison",
+  MyBloodyValentine: "Sometimes",
+};
 
 function updatePlaylist(playlist, artistName, songTitle){
   playlist [artistName] = songTitle;
